abdb792997364667a7f9cdab6d36805639081f53
Joshua Rodriguez <joshuaa.rodriguezr@hotmail.com>
2019-10-17 19:51:52 -0400

    Add project files.

---
+++ Models/ObjetoBibliotecaBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectLibrary.Models
+{
+        public abstract class ObjetoBibliotecaBase
+        {
+            public string Id { get; set; }
+            public virtual string Nombre { get; set; }
+
+            public ObjetoBibliotecaBase()
+            {
+                Id = Guid.NewGuid().ToString();
+            }
+
+            public override string ToString()
+            {
+                return $"{Nombre},{Id}";
+            }
+        }
+}
