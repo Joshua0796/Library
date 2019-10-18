949f5a8f16c3de1e24c4b8f783d1797cb5f96072
Joshua Rodriguez <joshuaa.rodriguezr@gmail.com>
2019-10-18 10:25:19 -0400

    Update ConexionController.cs

--- Controllers/ConexionController.cs
+++ Controllers/ConexionController.cs
@@ -4,27 +4,42 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Data.SqlClient;
-using Microsoft.EntityFrameworkCore.SqlServer;
 using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
-using Oracle.ManagedDataAccess.Client;
 
 namespace ProyectLibrary.Controllers
 {
     public class ConexionController
     {
         string connectionString = "Data Source=JOSHUAPC\\JOSHUAPC;Initial Catalog=BIBLIOTECA;Integrated Security=True";
-        SqlServerConnection cn = new SqlServerConnection(connectionString);
-
-        cn.Open();
-
-        return cn;
-
-
-        public void Dispose() 
+        SqlServerConnection cn = new SqlServerConnection();
+        
+        public static SqlServerConnection GetConnection()
         {
-            cn.Close();
+            //string connectionString = "Data Source=JOSHUAPC\\JOSHUAPC;Initial Catalog=BIBLIOTECA;Integrated Security=True";
+            //SqlServerConnection cn = new SqlServerConnection();
+            cn.Open();
             return cn;
         }
+        
+        public static void Dispose() 
+        {   
+            if(cn.State == System.Data.ConnectionState.Open)
+            {
+                cn.Close();
+                return cn;
+            }
+        }
+        
+        /*
+        public static void Dispose(SqlConnection cn) 
+        {   
+            if(cn.State == System.Data.ConnectionState.Open)
+            {
+                cn.Close();
+                return cn;
+            }
+        }
+        */
         
     }
 }
