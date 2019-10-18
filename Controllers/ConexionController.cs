4dffe34389987e3c03ca5b8798012af6e1f26187
Joshua Rodriguez <joshuaa.rodriguezr@gmail.com>
2019-10-18 11:33:39 -0400

    Update ConexionController.cs
    Changes the connection to Oracle Database

--- Controllers/ConexionController.cs
+++ Controllers/ConexionController.cs
@@ -1,32 +1,35 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Data.SqlClient;
-using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
+//using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
 
 namespace ProyectLibrary.Controllers
 {
     public class ConexionController
     {
-        string connectionString = "Data Source=JOSHUAPC\\JOSHUAPC;Initial Catalog=BIBLIOTECA;Integrated Security=True";
-        SqlServerConnection cn = new SqlServerConnection();
-        
-        public static SqlServerConnection GetConnection()
+        //string connectionString = "Data Source=JOSHUAPC\\JOSHUAPC;Initial Catalog=BIBLIOTECA;Integrated Security=True";
+        //SqlServerConnection cn = new SqlServerConnection(connectionString);
+
+        public static OracleConnection GetConnection()
         {
-            //string connectionString = "Data Source=JOSHUAPC\\JOSHUAPC;Initial Catalog=BIBLIOTECA;Integrated Security=True";
-            //SqlServerConnection cn = new SqlServerConnection();
+            string connectionString = "Data Source=JOSHUAPC\\JOSHUAPC;Initial Catalog=BIBLIOTECA;Integrated Security=True";
+            OracleConnection cn = new OracleConnection(connectionString);
+            
+            //SqlServerConnection cn = new SqlServerConnection(connectionString);
             cn.Open();
             return cn;
         }
         
-        public static void Dispose() 
+        public static void Dispose(OracleConnection cn) 
         {   
-            if(cn.State == System.Data.ConnectionState.Open)
+            if(cn.State == ConnectionState.Open)
             {
                 cn.Close();
-                return cn;
             }
         }
         
